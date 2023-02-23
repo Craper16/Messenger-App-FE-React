b760b6bd2898407bbcf064e56f3e432472cadda2
Georgio <georgio.saad@gmail.com>
2023-02-24 01:27:48 +0200

    attempting socket connection

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,15 +1,74 @@
-import React from 'react';
+import { Button, Input } from '@chakra-ui/react';
+import { useCallback, useEffect, useState } from 'react';
+import { connect } from 'socket.io-client';
+import { ACCESS_TOKEN } from '../../consts/constants';
 import { useAppSelector } from '../../redux/hooks';
 import { openWebSocketOnMount } from '../../utils/openWebSocketOnMount';
 
-export default function Home() {
-  const { displayName } = useAppSelector((state) => state.auth);
+interface MessageModel {
+  content: string;
+  sender: string;
+  receiver: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
 
-  openWebSocketOnMount();
+export default function Home() {
+  const [message, setMessage] = useState('');
+
+  const { displayName, userId } = useAppSelector((state) => state.auth);
+
+  const socket = connect(import.meta.env.VITE_API_KEY, {
+    query: { access_token: localStorage.getItem(ACCESS_TOKEN) },
+    reconnection: true,
+    rejectUnauthorized: true,
+  });
+
+  async function sendMessage() {
+    if (!message) return;
+
+    const messageData: MessageModel = {
+      content: message,
+      receiver: '63f15a1fe14c72f913df92f7',
+      sender: userId!,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+
+    await socket!.emit('send_message', messageData);
+    setMessage('');
+  }
+
+  // openWebSocketOnMount(localStorage.getItem(ACCESS_TOKEN)!);
+
+  useEffect(
+    useCallback(() => {
+      socket!.on('connect', () => {
+        console.log('We are here');
+      });
+
+      return () => {
+        socket!.disconnect();
+      };
+    }, []),
+    [socket]
+  );
+
+  useEffect(() => {
+    console.log('This is causing rerenders');
+    socket!.on('receive_message', (data: MessageModel) => {
+      console.log(data);
+    });
+  }, [socket]);
 
   return (
     <div>
       <div>{displayName}</div>
+      <Input
+        value={message}
+        onChange={(e) => setMessage(e.currentTarget.value)}
+      />
+      <Button onClick={sendMessage}>Send Message</Button>
     </div>
   );
 }
--- src/utils/openWebSocketOnMount.ts
+++ src/utils/openWebSocketOnMount.ts
@@ -1,11 +1,17 @@
 import { useEffect } from 'react';
 import { connect } from 'socket.io-client';
 
-export const openWebSocketOnMount = () =>
+export const openWebSocketOnMount = (access_token: string) =>
   useEffect(() => {
-    const socket = connect(import.meta.env.VITE_API_KEY);
+    const socket = connect(import.meta.env.VITE_API_KEY, {
+      query: { access_token: access_token },
+    });
 
     socket.on('connect', () => {
       console.log('We are here');
     });
+
+    // return () => {
+    //   socket.disconnect();
+    // };
   }, []);
